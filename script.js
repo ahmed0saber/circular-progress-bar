c5706f79495ccd17a81b625d2695b6ea4ee66940
Ahmed Saber <ahmed0saber33@gmail.com>
2022-07-23 23:59:02 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-let progressBars = document.getElementsByClassName("circle-progress")
-let circles = document.getElementsByClassName("progress-circle")
-let loadings = document.getElementsByClassName("loading")
+let progressBars = document.getElementsByClassName("circular-progress-bar")
+let progressedCircles = document.getElementsByClassName("progressed")
+let progressedPercentages = document.getElementsByClassName("progressed-percentage")
+let progressIntervals = []
+let animationCounters = []
 for(let i=0; i<progressBars.length; i++){
-    let circumference = circles[i].getTotalLength()
-    circles[i].style.strokeDashoffset = circumference - (progressBars[i].dataset.percentage / 100) * circumference
-    loadings[i].innerHTML = progressBars[i].dataset.percentage + "%"
+    let circumference = progressedCircles[i].getTotalLength()
+    animationCounters[i] = 0
+    progressIntervals[i] = setInterval(()=>{
+        animationCounters[i]++
+        progressedCircles[i].style.strokeDashoffset = circumference - (animationCounters[i] / 100) * circumference
+        progressedPercentages[i].innerHTML = animationCounters[i] + "%"
+        if(animationCounters[i] >= progressBars[i].dataset.percentage){
+            clearInterval(progressIntervals[i])
+        }
+    }, 2000/progressBars[i].dataset.percentage)
 }
